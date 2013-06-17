0710b6785c2c19501abbc4760584a98826083863
jcg <jcg@biofab>
2013-06-17 21:42:12 +0100

    Initial commit for D-Tailor v1.0

---
+++ Features/PWMScore.py
@@ -0,0 +1,56 @@
+'''
+Created on Nov 16, 2011
+
+@author: jcg
+'''
+
+from Features.Feature import Feature
+import Functions
+from uuid import uuid4
+
+class PWMScore(Feature):
+    """
+    PWMScore Feature
+        solution - solution where PWM score should be computed
+        label - some label to append to the name
+        pwm_range - start and end position to calculate PWM score - a tuple in the form (start, end)  
+        mutable_region - a list with all bases that can be mutated
+        cds_region - a pair with begin and end of CDSs - example: (0,100)
+        keep_aa - boolean option indicating if in the design mode amino acids should be kept        
+    """
+    def __init__(self, solution=None, label="", args = { 'pwm' : None,
+                                                         'pwm_range' : (0,9), 
+                                                         'mutable_region' : None, 
+                                                         'cds_regions' : None, 
+                                                         'keep_aa' : True }):
+        #General properties of feature
+        Feature.__init__(self, solution=solution, label=label)
+        #Specifics of this Feature
+        self.pwm                = args['pwm']
+        self.pwm_range          = args['pwm_range']
+        self.sequence           = solution.sequence[self.pwm_range[0]:self.pwm_range[1]+1]
+        self.mutable_region     = [i-self.pwm_range[0] for i in set(range(self.pwm_range[0],self.pwm_range[1]+1)) & set(args['mutable_region'])] if args.has_key('mutable_region') else solution.mutable_region
+        self.cds_region         = args['cds_region']    if args.has_key('cds_region') else solution.cds_region
+        self.keep_aa            = args['keep_aa']        if args.has_key('keep_aa') else solution.keep_aa        
+        self.set_scores()
+        self.set_level()
+
+    
+    def set_scores(self, scoring_function = Functions.analyze_pwm_score):
+        self.scores = Functions.appendLabelToDict(scoring_function(self.sequence,self.pwm), self.label)
+    
+    def mutate(self, operator=Functions.SimplePWMScoreOperator):
+        if not self.targetInstructions:
+            return None
+        
+        new_seq = list(self.solution.sequence)
+        mutated_seq = operator(self.sequence, self.pwm, self.targetInstructions['direction'], self.mutable_region, keep_aa=self.keep_aa)
+        if mutated_seq == None:
+            return None
+        else:
+            new_seq[self.ntusage_range[0]:self.ntusage_range[1]+1] = list(mutated_seq)
+        new_seq = "".join(new_seq) 
+                
+        return Solution.Solution(sol_id=str(uuid4().int), sequence=new_seq, cds_regions = self.cds_regions, mutable_region = self.mutable_region, parent=self.solution, design=self.solution.designMethod)
+    
+import Solution   
---
+++ Utils/DBKinetics.py
@@ -0,0 +1,55 @@
+'''
+Created on Apr 4, 2012
+
+@author: jcg
+'''
+
+from os import path
+from sqlite3 import connect,Row
+import sys
+
+
+def DBKinetics(db_file,resolution=50):
+    """
+        Prints a time series of desired solutions found as a function of generated solutions generated
+    """
+    #### Create connection to DB
+    con = connect(db_file)
+    con.isolation_level = None
+    con.row_factory = Row
+    cur = con.cursor()
+    
+    cur.execute("select count(1) from generated_solution")
+    total_sol = cur.fetchone()[0]
+
+    #print "Generated Solutions \t Desired Solutions Found"
+
+    #it = range(0,total_sol,total_sol/resolution)
+    it = range(0,total_sol,10)
+    it.append(total_sol)
+
+    #get statistics
+    for i in it:
+        cur.execute("select count(DISTINCT code) as c from (select des_solution_id as code from generated_solution LIMIT " + str(i) + ")")
+        result = cur.fetchone()
+    
+        #print i,"\t",result['c']
+        print result['c']
+    
+    con.close()
+    
+    
+if __name__ == '__main__':
+    pass
+    if len(sys.argv) == 2:
+        db_file = sys.argv[1]
+    else:
+        db_file = "../testFiles/outputFiles/tfec_ff_1.sqlite"
+        
+    for i in range(1,31):
+        db_file = "../testFiles/outputFiles/tfec_ff_rnd_samp_s"+str(i)+".sqlite"
+        print "seed"+str(i)+",end"
+        DBKinetics(db_file)
+    
+    #One-time
+    #DBKinetics(db_file)
