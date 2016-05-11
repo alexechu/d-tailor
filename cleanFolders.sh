b899faad0251dc5f6377afbe63279d12a916c2c3
Shyam Saladi <saladi@caltech.edu>
2016-05-11 14:07:57 -0700

    move clean tmp folder script

---
+++ cleanFolders.sh
@@ -0,0 +1,2 @@
+# remove files but not directory structure
+find ./tmp -type f | xargs rm
--- tmp/cleanFolders.sh
+++
@@ -1,6 +0,0 @@
-cd structures/
-find . -name '*' | xargs rm 
-cd ../unafold_files/
-find . -name '*' | xargs rm 
-cd ..
-rm -rf transterm_files/*
