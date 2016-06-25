9c5de51420d061c8978ba4cf80915cf7a0b1e86b
Alex Chu <alexechu1@gmail.com>
2016-06-25 12:57:30 -0700

    Created a fake RNAss class

---
+++ Features/RNAss.py
@@ -0,0 +1,11 @@
+class RNAss(Feature):
+
+    def __init__(name, featuresList, color, solution):
+        self.name = name
+        self.featuresList = featuresList
+        self.color = color
+        self.solution = solution
+
+
+    def configureSolution():
+        pass
